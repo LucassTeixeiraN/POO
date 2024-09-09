23b9101d656429fd5cd3a8840e5970ee5bc6bcc0
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-09-09 08:06:10 -0300

    Update 16.py

--- ML1/16.py
+++ ML1/16.py
@@ -1,19 +1,6 @@
 # 16. Sendo
-# S
-
-# N
-# = 1− + − + − +
-# 1
-# 2
-# 1
-# 3
-# 1
-# 4
-# 1
-# 5
-# 1
-# ... , construa um programa que leia N, calcule e mostre
-
+# S = 1/2 + 1/3 + 1/4 + ... + 1/N 
+#construa um programa que leia N, calcule e mostre
 # o valor da série S.
 
 
