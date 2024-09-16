a5dade8b3bd8c6d7506d246340e380be0277bb9a
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-09-16 15:22:38 -0300

    Update 4.py

--- M3L/4.py
+++ M3L/4.py
@@ -2,17 +2,23 @@
 que exiba o name e o balance de uma conta passada como parâmetro. Altere também
 o main para, ao invés de imprimir diretamente o name e o balance da conta, use o
 método displayAccount, passando a conta como parâmetro.'''
+import locale
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 class Account:
     def __init__(self, name, saldo):
         self.saldo = saldo
         self.name = name
 
-    def displayAccount(self):
-        print(f"Nome: {self.name}, Saldo: {self.saldo:.2f}")
+    def displayAccount(self, debito):
+        if debito > self.saldo:
+            print('Quantia de débito excedeu o saldo da conta.')
+        else:
+            self.saldo -= debito
+            print(f"Nome: {self.name}, Saldo disponível: {locale.currency(self.saldo, grouping=True)}")
 
 def main():
     conta = Account("Herba", 1000)
-    conta.displayAccount()
+    conta.displayAccount(10000)
 
 main()
