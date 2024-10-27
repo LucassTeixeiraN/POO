dcd1a686e9ab593c5d4ad7589e44455d96b3a123
João_Santos <164881489+jptrava@users.noreply.github.com>
2024-10-27 14:57:15 -0300

    Update 2.py

--- M9L/M3L/2.py
+++ M9L/M3L/2.py
@@ -1,31 +1,4 @@
-class Bike:
-    def __init__(self, velocidade, marcha, cadencia, numSerie):
-        self.velocidade = velocidade
-        self.marcha = marcha
-        self.cadencia = cadencia
-        self.numSerie = numSerie
-
-    def get_velocidade(self):
-        return self.velocidade
-
-    def __str__(self):
-        return (f"Bicicleta {self.numSerie}: Velocidade = {self.velocidade}, "
-                f"Marcha = {self.marcha}, Cadência = {self.cadencia}")
-
-class Comparador:
-    def comparar(self, bicicleta1, bicicleta2):
-        raise NotImplementedError("Método deve ser implementado pela subclasse.")
-
-class ComparadorPorVelocidade(Comparador):
-    def comparar(self, bicicleta1, bicicleta2):
-        if bicicleta1.get_velocidade() < bicicleta2.get_velocidade():
-            return "A velocidade da Bicicleta 2 é maior que a da Bicicleta 1"
-        elif bicicleta1.get_velocidade() == bicicleta2.get_velocidade():
-            return "A velocidade da Bicicleta 1 é igual à da Bicicleta 2"
-        else:
-            return "A velocidade da Bicicleta 1 é maior que a da Bicicleta 2"
-
-class SistemaDeBicicletas:
+class SistemaDeBicicletas:  # Classe gerenciadora
     def __init__(self):
         self.bicicletas = []
 
@@ -40,28 +13,53 @@ class SistemaDeBicicletas:
                 if (velocidade < 0 or marcha < 0 or marcha > 18 or cadencia < 0 or numSerie < 1000):
                     print("Algum(ns) valor(es) estão incorreto(s). Favor preencha-os corretamente!")
                 else:
-                    bike = Bike(velocidade, marcha, cadencia, numSerie)
+                    bike = Bike(velocidade, marcha, cadencia, numSerie, self)  
                     self.bicicletas.append(bike)
                     break
             except ValueError:
                 print("Insira um número válido!")
 
-    def comparar_bicicletas(self):
-        if len(self.bicicletas) < 2:
-            print("É necessário criar pelo menos duas bicicletas para comparação.")
-            return
-        
-        comparador = ComparadorPorVelocidade()
-        resultado = comparador.comparar(self.bicicletas[0], self.bicicletas[1])
-        
-        print(resultado)
-        for bike in self.bicicletas:
+    def adicionar_bicicletas(self):
+        while True:
+            try:
+                qtd = int(input("Quantas bicicletas você deseja adicionar? "))
+                if qtd <= 0:
+                    print("Por favor, insira um número positivo.")
+                else:
+                    for i in range(1, qtd + 1):
+                        self.criar_bike(i)
+                    break
+            except ValueError:
+                print("Insira um número válido!")
+
+    def mostrar_bicicletas_ordenadas(self):
+        bicicletas_ordenadas = sorted(self.bicicletas, key=lambda bike: bike.get_velocidade())
+        print("\nBicicletas ordenadas por velocidade:")
+        for bike in bicicletas_ordenadas:
             print(bike)
+
+class Bike:  
+    def __init__(self, velocidade, marcha, cadencia, numSerie, sistema):
+        self.velocidade = velocidade
+        self.marcha = marcha
+        self.cadencia = cadencia
+        self.numSerie = numSerie
+        self.sistema = sistema  #Referência ao sistema
+
+    def get_velocidade(self):
+        return self.velocidade
+
+    def __str__(self):
+        return (f"Bicicleta {self.numSerie}: Velocidade = {self.velocidade}, "
+                f"Marcha = {self.marcha}, Cadência = {self.cadencia}")
+
+    def mostrar_informacoes(self):
+        print(f"Informações da {self}:")
+        self.sistema.mostrar_bicicletas_ordenadas()  #Chama método do sistema
 
 def main():
     sistema = SistemaDeBicicletas()
-    sistema.criar_bike("1")
-    sistema.criar_bike("2")
-    sistema.comparar_bicicletas()
+    sistema.adicionar_bicicletas()
+    sistema.mostrar_bicicletas_ordenadas()
 
 main()
