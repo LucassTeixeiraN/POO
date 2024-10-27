7ee4df77f5e3a2ff51039110242989b47f55120c
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-10-27 16:23:41 -0300

    m9l m3l 2 changes

--- M9L/M3L/2.py
+++ M9L/M3L/2.py
@@ -2,35 +2,8 @@ class SistemaDeBicicletas:
     def __init__(self):
         self.bicicletas = []
 
-    def criar_bike(self, num):
-        while True:
-            try:
-                velocidade = float(input(f"Insira a velocidade da bicicleta {num}: "))
-                marcha = int(input(f"Insira a marcha atual da bicicleta {num}: "))
-                cadencia = int(input(f"Insira a cadência (RPM) da bike {num}: "))
-                numSerie = int(input(f"Insira o número de série da bicicleta {num}: "))
-                
-                if (velocidade < 0 or marcha < 0 or marcha > 18 or cadencia < 0 or numSerie < 1000):
-                    print("Algum(ns) valor(es) estão incorreto(s). Favor preencha-os corretamente!")
-                else:
-                    bike = Bike(velocidade, marcha, cadencia, numSerie, self)  
-                    self.bicicletas.append(bike)
-                    break
-            except ValueError:
-                print("Insira um número válido!")
-
-    def adicionar_bicicletas(self):
-        while True:
-            try:
-                qtd = int(input("Quantas bicicletas você deseja adicionar? "))
-                if qtd <= 0:
-                    print("Por favor, insira um número positivo.")
-                else:
-                    for i in range(1, qtd + 1):
-                        self.criar_bike(i)
-                    break
-            except ValueError:
-                print("Insira um número válido!")
+    def appendBike(self, bike):
+        self.bicicletas.append(bike)
 
     def mostrar_bicicletas_ordenadas(self):
         bicicletas_ordenadas = sorted(self.bicicletas, key=lambda bike: bike.get_velocidade())
@@ -39,15 +12,18 @@ class SistemaDeBicicletas:
             print(bike)
 
 class Bike:  
-    def __init__(self, velocidade, marcha, cadencia, numSerie, sistema):
+    def __init__(self, velocidade, marcha, cadencia, numSerie):
         self.velocidade = velocidade
         self.marcha = marcha
         self.cadencia = cadencia
         self.numSerie = numSerie
-        self.sistema = sistema  #Referência ao sistema
+        self.sistema = None
 
     def get_velocidade(self):
         return self.velocidade
+
+    def set_sistema(self, sistema: SistemaDeBicicletas):
+        self.sistema = sistema
 
     def __str__(self):
         return (f"Bicicleta {self.numSerie}: Velocidade = {self.velocidade}, "
@@ -57,9 +33,43 @@ class Bike:
         print(f"Informações da {self}:")
         self.sistema.mostrar_bicicletas_ordenadas()  #Chama método do sistema
 
+
+def criar_bike(num):
+    while True:
+        try:
+            velocidade = float(input(f"Insira a velocidade da bicicleta {num}: "))
+            marcha = int(input(f"Insira a marcha atual da bicicleta {num}: "))
+            cadencia = int(input(f"Insira a cadência (RPM) da bike {num}: "))
+            numSerie = int(input(f"Insira o número de série da bicicleta {num}: "))
+            
+            if (velocidade < 0 or marcha < 0 or marcha > 18 or cadencia < 0 or numSerie < 1000):
+                print("Algum(ns) valor(es) estão incorreto(s). Favor preencha-os corretamente!")
+            else:
+                bike = Bike(velocidade, marcha, cadencia, numSerie)
+                return bike
+        except ValueError:
+            print("Insira um número válido!")
+
+
+def adicionar_bicicletas(sistema):
+    while True:
+        try:
+            qtd = int(input("Quantas bicicletas você deseja adicionar? "))
+            if qtd <= 0:
+                print("Por favor, insira um número positivo.")
+            else:
+                for i in range(1, qtd + 1):
+                    bike = criar_bike(i)
+                    sistema.appendBike(bike)
+                    bike.set_sistema(sistema)
+                break
+        except ValueError:
+            print("Insira um número válido!")
+
 def main():
     sistema = SistemaDeBicicletas()
-    sistema.adicionar_bicicletas()
+
+    adicionar_bicicletas(sistema)
     sistema.mostrar_bicicletas_ordenadas()
 
 main()
