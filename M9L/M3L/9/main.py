5cd230556ddad94cd3c9167df3a4054ff601a7d6
Andrey Rodrigues <134998417+AndreyRodr@users.noreply.github.com>
2024-10-28 08:47:39 -0300

    Update main.py

--- M9L/M3L/9/main.py
+++ M9L/M3L/9/main.py
@@ -4,25 +4,25 @@ from event import Event
 def main():
     while True:
         try:
-            
             nome_evento = input("Insira o nome do evento: ")
 
-            # Instanciando a classe Date
             dia_inicial = int(input("Insira o dia inicial: "))
             mes_inicial = int(input("Insira o mês inicial: "))
             ano_inicial = int(input("Insira o ano inicial: "))
             
             date = Date(dia_inicial, mes_inicial, ano_inicial)
-            evento = Event(nome_evento, date)  # Associa o evento à nova data
+            evento = Event(nome_evento, date) 
             print(f"\nData inicial do evento '{evento.getNome()}':", date)
 
-            # Alterando a data
+         
             date.setDia(int(input("\nInsira o novo dia do evento: ")))
             date.setMes(int(input("Insira o novo mês do evento: ")))
             date.setAno(int(input("Insira o novo ano do evento: ")))
-            # Testando a classe Event
+
+        
             print()
             print(evento)
+            print("Evento associado à data:", date.getEvento().getNome())
             break
         except ValueError:
             print("Insira valores válidos.")
