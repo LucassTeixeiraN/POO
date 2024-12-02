6b8f529cd6fbc78a8230f65ca43f2e0b69526208
João Paulo Andrade Raimundo <62303009+jpegame@users.noreply.github.com>
2024-12-01 22:57:17 -0300

    a

--- M16L/ex6/college.py
+++ M16L/ex6/college.py
@@ -1,10 +1,11 @@
 from students import Student
+from typing import Type
 
 class College:
     def __init__(self):
         self.students: list[Student] = []
 
-    def add_student(self, student):
+    def add_student(self, student: Type[Student]):
         if isinstance(student, Student):
             self.students.append(student)
         else:
